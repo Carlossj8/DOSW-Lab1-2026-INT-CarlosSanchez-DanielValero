c59105425573873530c24ffaceb146244c93a417
Mainskull <danielvalero20059@gmail.com>
2026-07-30 10:04:52 -0500

    Creacion de carpeta de laboratorio con subcarpetas por reto

---
+++ Laboratorio1/reto1/Estudiante.java
@@ -0,0 +1,15 @@
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private int Semestre;
+    private String correo;
+
+    public Estudiante(String nombre, int edad, int Semestre, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.Semestre = Semestre;
+        this.correo = correo;
+
+    }
+
+}
