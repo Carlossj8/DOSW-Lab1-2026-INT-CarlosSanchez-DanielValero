7d5cf08e3967c5232b39ac9a3e11e19b054313b9
Mainskull <daniel.valero-q@mail.escuelaing.edu.co>
2026-08-04 15:58:22 -0500

    Reto 1 completado

--- Laboratorio1/reto1/Estudiante.java
+++ Laboratorio1/reto1/Estudiante.java
@@ -1,15 +1,46 @@
+package Laboratorio1.reto1;
+
 public class Estudiante {
     private String nombre;
     private int edad;
-    private int Semestre;
+    private String Semestre;
     private String correo;
 
-    public Estudiante(String nombre, int edad, int Semestre, String correo) {
+    public Estudiante(String nombre, int edad, String Semestre, String correo) {
         this.nombre = nombre;
         this.edad = edad;
         this.Semestre = Semestre;
         this.correo = correo;
 
     }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSemestre() {
+        return Semestre;
+    }
+    public void setSemestre(String semestre) {
+        this.Semestre = semestre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 
 }
--- Laboratorio1/reto1/Reto1.java
+++ Laboratorio1/reto1/Reto1.java
@@ -1,3 +1,45 @@
-public class Reto1 {
+package Laboratorio1.reto1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Reto1 {
+    private static ArrayList<Estudiante> estudiantes;
+
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Introduce el numero de estudiantes");
+        estudiantes = new ArrayList<>();
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.println("Nombre del estudiante");
+            String nombre = scanner.nextLine();
+
+            System.out.println("Semestre");
+            String semestre = scanner.nextLine();
+
+            System.out.println("Edad");
+            int edad = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Correo");
+            String correo = scanner.nextLine();
+
+            Estudiante estudiante = new Estudiante(nombre, edad, semestre, correo);
+            estudiantes.add(estudiante);
+
+        }
+        scanner.close();
+
+        MensajeBienvenida mensaje = new MensajeBienvenida();
+        mensaje.imprimirMensaje(estudiantes);
+    }
+
+    public static ArrayList<Estudiante> getEstudiantes(){
+        return estudiantes;
+
+    }
 }
