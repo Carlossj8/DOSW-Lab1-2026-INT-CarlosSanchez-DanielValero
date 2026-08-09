f4555613b85958f46e5be86bc0dadebf11cad27c
carlos <charliesj898@gmail.com>
2026-08-08 22:53:09 -0500

    Reto#2 culminado

--- Laboratorio1/reto2/Reto2.java
+++ Laboratorio1/reto2/Reto2.java
@@ -8,16 +8,34 @@ public class Reto2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Integer> lista = new ArrayList<>();
+        Carril1 carril1 = new Carril1();
 
-        System.out.print("¿Cuántos números deseas ingresar a la lista?: ");
-        int n = scanner.nextInt();
-
-        System.out.println("Ingresa los " + n + " números:");
-        for (int i = 0; i < n; i++) {
-            lista.add(scanner.nextInt());
+        System.out.print("¿Cuántos números deseas ingresar para la Lista 1?: ");
+        int n1 = scanner.nextInt();
+        List<Integer> lista1 = new ArrayList<>();
+        System.out.println("Ingresa los " + n1 + " números de la Lista 1:");
+        for (int i = 0; i < n1; i++) {
+            lista1.add(scanner.nextInt());
         }
 
+        System.out.print("\n¿Cuántos números deseas ingresar para la Lista 2?: ");
+        int n2 = scanner.nextInt();
+        List<Integer> lista2 = new ArrayList<>();
+        System.out.println("Ingresa los " + n2 + " números de la Lista 2:");
+        for (int i = 0; i < n2; i++) {
+            lista2.add(scanner.nextInt());
+        }
+
+        System.out.println("\n--- RESULTADOS ---");
+
+        procesarLista(lista1, "Lista 1", carril1);
+        System.out.println();
+        procesarLista(lista2, "Lista 2", carril1);
+
+        scanner.close();
+    }
+
+    private static void procesarLista(List<Integer> lista, String nombreLista, Carril1 carril1) {
         int menor = lista.stream()
                 .min((a, b) -> a.compareTo(b))
                 .orElse(0);
@@ -25,22 +43,20 @@ public class Reto2 {
         long cantidad = lista.stream()
                 .count();
 
-        Carril1 carril1 = new Carril1();
-
         int maximo = carril1.calcularMaximo(lista);
+
         boolean maximoEsMultiploDeDos = carril1.esMultiploDeDos(maximo);
-
         boolean esDivisorDe2 = (maximo != 0) && (2 % maximo == 0);
+        boolean esPar = cantidad % 2 == 0;
 
-        System.out.println("\n--- Resultados Combinados ---");
-        System.out.println("Mínimo: " + menor);
-        System.out.println("Máximo: " + maximo);
-        System.out.println("Cantidad de datos ingresados: " + cantidad);
-        System.out.println("¿El máximo es múltiplo de 2?: " +
-                (maximoEsMultiploDeDos ? "Sí" : "No"));
-        System.out.println("¿El máximo es divisor de 2?: " +
-                (esDivisorDe2 ? "Sí" : "No"));
+        System.out.println(nombreLista + " => max:" + maximo + " min:" + menor + " cant: " + cantidad);
+        System.out.println("¿" + maximo + " múltiplo de 2? " + (maximoEsMultiploDeDos ? "Si" : "No"));
+        System.out.println("¿" + maximo + " divisor de 2? " + (esDivisorDe2 ? "Si" : "No"));
 
-        scanner.close();
+        if (esPar) {
+            System.out.println("¿Cantidad (" + cantidad + ") es par? Si");
+        } else {
+            System.out.println("¿Cantidad (" + cantidad + ") es impar? Si");
+        }
     }
 }
