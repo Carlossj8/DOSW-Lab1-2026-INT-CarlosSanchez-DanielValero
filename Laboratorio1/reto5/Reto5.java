f7973ea2d3c2a1fee284a2eb2f8eede9704a4112
carlos <charliesj898@gmail.com>
2026-08-09 00:14:18 -0500

    Parte inicial Estudiante A Reto5

--- Laboratorio1/reto5/Reto5.java
+++ Laboratorio1/reto5/Reto5.java
@@ -1,4 +1,20 @@
 package Laboratorio1.reto5;
 
+import java.util.Scanner;
+import java.util.Set;
+
 public class Reto5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Estudiante A: HashSet ===");
+        EstudianteA estudianteA = new EstudianteA();
+        Set<Integer> hashSetFiltrado = estudianteA.crearHashSetSinMultiplosDe3(scanner);
+
+        System.out.println("\n=== Resultado HashSet sin múltiplos de 3 ===");
+        hashSetFiltrado.forEach(n -> System.out.println("Número en arena: " + n));
+
+        scanner.close();
+    }
 }
