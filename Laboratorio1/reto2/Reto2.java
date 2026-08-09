edbaf392716e3862b6662700e17c7b69b7473b5f
Mainskull <daniel.valero-q@mail.escuelaing.edu.co>
2026-08-08 21:33:49 -0500

    Carril 1 con funcion main en la clase Reto 2

--- Laboratorio1/reto2/Reto2.java
+++ Laboratorio1/reto2/Reto2.java
@@ -1,4 +1,17 @@
 package Laboratorio1.reto2;
 
+import java.util.List;
+
 public class Reto2 {
+
+    public static void main(String[] args) {
+
+        List<Integer> lista1 = List.of(14, 3, 27, 8, 51, 6);
+
+        Carril1 carril1 = new Carril1();
+
+        int maximo = carril1.calcularMaximo(lista1);
+
+        System.out.println("Máximo: " + maximo);
+    }
 }
