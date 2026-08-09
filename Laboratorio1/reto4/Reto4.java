c74b6b220b3d9776e9491e4b92fa9482bc264cf6
carlos <charliesj898@gmail.com>
2026-08-08 23:50:22 -0500

    Parte inicial estudianteA reto4

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -1,4 +1,26 @@
 package Laboratorio1.reto4;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Reto4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        EstudianteA estudianteA = new EstudianteA();
+        Map<String, Integer> hashMap = estudianteA.crearHashMap(scanner);
+
+        String resultado = hashMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry -> "Clave: " + entry.getKey().toUpperCase()
+                        + " | Valor: " + entry.getValue())
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(resultado);
+
+        scanner.close();
+    }
 }
